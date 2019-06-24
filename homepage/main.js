b57c0ce98b6f09630b8a4b11a88257de40adf83a
Paweł Dąbała <pawel.dabala@global.com>
2019-06-24 09:06:01 +0200

    chenging class names to BEM specification

--- homepage/main.js
+++ homepage/main.js
@@ -4,4 +4,13 @@ const age = 31;
 console.log(name);
 console.log(age)
 
-console.log(`Nazywam się ${name} i mam ${age} lat.`)
+console.log(`Nazywam się ${name} i mam ${age} lat.`);
+
+const about = document.querySelector('.about__paragraph--js');
+console.log(about.innerHTML);
+
+about.innerHTML = `<strong>Nazywam</strong> się ${name} i mam ${age} lat.`;
+
+const paragraphs = document.querySelectorAll('p');
+console.log(paragraphs);
+console.log(paragraphs[0]);
