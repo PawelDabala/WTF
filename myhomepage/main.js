082fce3f1a0c95127f7de9e5135b70b61c62dfc9
Paweł Dąbała <pawel.dabala@global.com>
2019-06-24 11:20:03 +0200

    added js script to add new text

--- myhomepage/main.js
+++ myhomepage/main.js
@@ -1,2 +1,6 @@
 let name = 'Pawel'
 // alert(`Cześć dzięki za odwiedzenie strony, ${name}.`)
+
+const entry = document.querySelector('.entry__paragraph--js');
+console.log(entry.innerHTML);
+entry.innerHTML = 'It is said that the SQL is a standard language for accessing databases. In truth, the SQL syntax varies from one database to another. Databases vendors rarely stick to the standard and prefer to add their own twist and turn to the language. For example, if we are using Firebird then the SQL query to select id and name of the first 5 rows from the employees table would be:'
