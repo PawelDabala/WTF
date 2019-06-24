23453f24c19cc62d3d5e4f324e5e83a4aa2eb498
Paweł Dąbała <pawel.dabala@global.com>
2019-06-24 15:31:09 +0200

    before chenge navigation

--- homepage/main.js
+++ homepage/main.js
@@ -1,6 +1,6 @@
 console.log('siema');
 const name = 'Maciek';
-const age = 31;
+const age = 80;
 console.log(name);
 console.log(age)
 
@@ -14,3 +14,38 @@ about.innerHTML = `<strong>Nazywam</strong> się ${name} i mam ${age} lat.`;
 const paragraphs = document.querySelectorAll('p');
 console.log(paragraphs);
 console.log(paragraphs[0]);
+
+if ('javasript' != 'java') {
+    console.log('to prawda!!')
+}
+
+const amIOld = (age > 70) ? 'yes' : 'no';
+console.log(amIOld)
+
+let oldIndicator;
+
+if (age > 70) {
+    oldIndicator = 'yes';
+} else {
+    oldIndicator = 'no';
+}
+
+function calculate(x) {
+    x = x + 3;
+    console.log('tradycyjnie: ${x}');
+    return x*7
+}
+
+console.log(calculate(2));
+const mycalculate = calculate(4);
+console.log(mycalculate);
+
+// nowa sposób zapisu funkcji:
+// const calculateFat = (x) => {
+//     x = x + 3;
+//     console.log('fat: ${x}');
+//     return x*7
+// }
+const calculateFat = x => (x+3)*7;
+
+console.log(calculateFat(2));
