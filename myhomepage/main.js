634a398a8bdb087ef22eb09a77f1999acbe9f12d
Paweł Dąbała <pawel.dabala@global.com>
2019-07-09 15:40:01 +0200

    add wizytowka home work

--- myhomepage/main.js
+++ myhomepage/main.js
@@ -1,6 +1,22 @@
-let name = 'Pawel'
+// let name = 'Pawel'
 // alert(`Cześć dzięki za odwiedzenie strony, ${name}.`)
 
-const entry = document.querySelector('.entry__paragraph--js');
-console.log(entry.innerHTML);
-entry.innerHTML = 'It is said that the SQL is a standard language for accessing databases. In truth, the SQL syntax varies from one database to another. Databases vendors rarely stick to the standard and prefer to add their own twist and turn to the language. For example, if we are using Firebird then the SQL query to select id and name of the first 5 rows from the employees table would be:'
+// const entry = document.querySelector('.entry__paragraph--js');
+// console.log(entry.innerHTML);
+// entry.innerHTML = 'It is said that the SQL is a standard language for accessing databases. In truth, the SQL syntax varies from one database to another. Databases vendors rarely stick to the standard and prefer to add their own twist and turn to the language. For example, if we are using Firebird then the SQL query to select id and name of the first 5 rows from the employees table would be:'
+
+let name = prompt("Podaj imie:")
+let secendname = prompt("Podaj nazwisko:")
+
+
+
+// const show_name = (name1, secendname1) => {
+//     // console.log("super")
+//     alert(`Witaj ${name1} ${secendname1}.`)
+// }
+
+const show_name = (name, secname) => alert(`Witaj ${name} ${secname}.`)
+
+show_name(name, secendname) 
+
+// alert(`Witaj ${name} ${secendname}.`)
